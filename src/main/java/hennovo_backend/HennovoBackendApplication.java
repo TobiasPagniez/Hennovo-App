16f54e7c8e874a241ffbec8f6ab30879d046666e
Nazareno Luna <nazarenoluna86@gmail.com>
2026-08-02 10:00:11 -0300

    feat: initialize Spring Boot backend project

---
+++ src/main/java/hennovo_backend/HennovoBackendApplication.java
@@ -0,0 +1,13 @@
+package hennovo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HennovoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HennovoBackendApplication.class, args);
+	}
+
+}
