67c2bb32fcf3d29d55f11896c4346ff63ed4ec2b
valentinolm <vlmvalenlozanomassey@gmail.com>
2026-08-05 23:43:14 -0300

    repositorio de producto agregado

---
+++ src/main/java/hennovo_backend/productos/repository/ProductoRepository.java
@@ -0,0 +1,19 @@
+package hennovo_backend.productos.repository;
+
+import hennovo_backend.productos.entity.Producto;
+import hennovo_backend.productos.enums.Presentacion;
+import hennovo_backend.productos.enums.Tamaño;
+import hennovo_backend.productos.enums.TipoHuevo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    List<Producto> findByActivoTrue();
+
+    boolean existsByTipoHuevoAndTamanoAndPresentacion(
+            TipoHuevo tipoHuevo,
+            Tamaño tamaño,
+            Presentacion presentacion
+    );
+}
